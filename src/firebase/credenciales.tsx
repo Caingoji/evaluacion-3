74c025e9286ab2300702f6ffbec9a0229e5b4435
Javier Contreras <javiercontrerasgallardo@gmail.com>
2025-07-11 19:54:44 -0400

    prueba para hacer los primeros pasos
    De momento solo surguieron ciertos errores al momento de ingresar pero ya se solucionaro
    
    y ahora pondre a prueba si funciona el branch

---
+++ src/firebase/credenciales.tsx
@@ -0,0 +1,11 @@
+// credenciales.ts o credenciales.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAHR_jvV4zxyyioUWzKtA23zAz71SGrnsI",
+  authDomain: "proto-2833c.firebaseapp.com",
+  projectId: "proto-2833c",
+  storageBucket: "proto-2833c.appspot.com",
+  messagingSenderId: "227132817296",
+  appId: "1:227132817296:web:0a5f54b7f32c4308f77d15",
+  measurementId: "G-2J290911M9"
+};
